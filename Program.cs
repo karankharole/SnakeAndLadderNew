5b2d8b78c2b2cbebcc47252b14896d05bb600d66
karankharole <karankharole2000@gmail.com>
2023-01-10 07:36:40 +0530

    [karan] starting with single player

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadderNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SnakeAndLadder SnakeAndLadderObj = new SnakeAndLadder();
+
+            SnakeAndLadderObj.singlePlayer();
+        }
+    }
+}
---
+++ SnakeAndLadder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderNew
+{
+    internal class SnakeAndLadder
+    {
+
+        public void singlePlayer()
+        {
+            int num_Of_Player = 1;
+            int start_Position = 0;
+
+            Console.WriteLine("Starting with only " + num_Of_Player + " player");
+            Console.WriteLine("First player position is : " + start_Position);
+
+        }
+    }
+}
